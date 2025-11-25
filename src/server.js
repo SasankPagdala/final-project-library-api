c8b70fa94f1c8765d72b4058079d5c9365211676
Ilton da Costa Neto <idacosta@charlotte.edu>
2025-11-24 22:13:39 -0500

    Add list sharing OpenAPI documentation

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const swaggerDocument = YAML.load(join(__dirname, '../docs/openapi.yaml'));
-
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 import authRoutes from './routes/authRoutes.js';
 import listRoutes from './routes/listRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
